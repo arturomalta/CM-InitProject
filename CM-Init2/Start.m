30d9e8db91af88d1e4a33ee76587129a06b595ce
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-06-11 20:32:25 -0500

    Array added to the name label

--- CM-Init2/Start.m
+++ CM-Init2/Start.m
@@ -5,27 +5,41 @@
 //  Created by Walter Gonzalez Domenzain on 11/06/15.
 //  Copyright (c) 2015 Smartplace. All rights reserved.
 //
-
 #import "Start.h"
+
+NSMutableArray  *maNames;
+
+int iIndex = 0;
 
 @interface Start ()
 
 @end
 
 @implementation Start
-
+/**********************************************************************************************/
+#pragma mark - Init methods
+/**********************************************************************************************/
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.lblWelcome.text    = @"Que calor!";
+    [self initController];
 }
-
+//-------------------------------------------------------------------------------
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+//-------------------------------------------------------------------------------
+- (void)initController {
+    self.lblWelcome.text    = @"Names";
+    maNames           = [[NSMutableArray alloc] init];
+    maNames           = [[NSMutableArray alloc] initWithObjects: @"Walter", @"Carlos", @"Vita", @"Carlos", @"Diego", nil];
+}
+/**********************************************************************************************/
+#pragma mark - Button methods
+/**********************************************************************************************/
 - (IBAction)btn1Pressed:(id)sender {
-    self.lblWelcome.text                        = @"Que sed, y no de agua!";
+    self.lblWelcome.text                        = maNames[iIndex];
+    iIndex++;
     self.lblWelcome.adjustsFontSizeToFitWidth   = YES;
 }
 @end
